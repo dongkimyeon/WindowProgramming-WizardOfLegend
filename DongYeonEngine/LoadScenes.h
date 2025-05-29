65eedb3b7dc7a44ba4e55cab50eeb26d5d28a3e8
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-05-30 03:20:08 +0900

    최근저장
    ㅁㄴㅇ

--- DongYeonEngine/LoadScenes.h
+++ DongYeonEngine/LoadScenes.h
@@ -22,7 +22,7 @@ void LoadScenes()
 	SceneManager::CreateScene<GameClearScene>(L"GameClearScene");
 
 	//MapManager::GetInstance()->LoadMap(L"StageBoss.txt");
-	SceneManager::LoadScene(L"Stage1");
+	SceneManager::LoadScene(L"TitleScene");
 	SceneManager::GetSharedPlayer()->SetPosition(1025, 1850);
 	SoundManager::GetInstance()->Initialize();
 	SoundManager::GetInstance()->mPlaySound("TitleScreen", true);
